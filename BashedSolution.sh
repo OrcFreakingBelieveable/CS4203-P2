6f76dc6f3b04b13e29fd3a70d31353b65abf705b
190021081 <gjc6@st-andrews.ac.uk>
2021-10-28 18:13:51 +0100

    Bash solution started

---
+++ BashedSolution.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+letters=()
+millis=()
+echo "Enter a password"
+# read a single character
+while read -n1 key; do
+    # if key = ENTER key
+    if [ $key == $'\e' ]; then
+        break
+    fi
+    letters=(${letters[@]} $key)
+    millis=(${millis[@]} $(date +%s%3N))
+done
+echo ""
+for ((i = 0; i < ${#letters[@]} - 1; i++)); do
+    delay=$((${millis[i+1]} - ${millis[i]}))
+    echo "${letters[i]}${letters[i+1]}: ${delay}"
+done
---
+++ timings/template.profile
@@ -0,0 +1,5 @@
+# the template for Keystroke Dynamic profile 
+File name: 
+<Volunteer id: int>_<Test Code: string> # e.g. V1_1.a
+File content: 
+<Volunteer id: int> "<Test Code: string>" "<ID: String>" <ID Timings: CS Array> "<Password: String>" <Password Timings: CS Array>
