07c3e7d189f9fb5cd003d0ea27ba561eda612a42
190021081 <gjc6@st-andrews.ac.uk>
2021-10-30 13:44:56 +0100

    More testing

--- BashedSolution.sh
+++ BashedSolution.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
-letters=()
-millis=()
+declare id="" # string of at most 10 characters 
+declare -a id_millis # the <=9 millisecond differences between the <=10 letters 
+declare pw # string of at most 20 characters 
+declare -a pw_millis # the <=19 millisecond differences between the <=20 letters 
+volunteer=$1
+test_code=$2
+index=0
+
+echo "Enter a user name"
+# read a single character
+while read -n1 key; do
+    # if key = Escape key
+    if [ $key == $'\e' ]; then
+        break
+    fi
+
+    id=$id$key
+    id_millis[$index]=$(date +%s%3N)
+
+    ((index+=1))
+    if [ $index -gt 9 ]; then 
+        break
+    fi
+done
+
+index=0
+echo ""
+
 echo "Enter a password"
 # read a single character
 while read -n1 key; do
@@ -8,13 +34,23 @@ while read -n1 key; do
     if [ $key == $'\e' ]; then
         break
     fi
-    letters=(${letters[@]} $key)
-    millis=(${millis[@]} $(date +%s%3N))
+
+    pw=$id$key
+    pw_millis[$index]=$(date +%s%3N)
+
+    ((index+=1))
+    if [ $index -gt 19 ]; then 
+        break
+    fi
 done
-echo ""
-for ((i = 0; i < ${#letters[@]} - 1; i++)); do
-    delay=$((${millis[i+1]} - ${millis[i]}))
-    echo "${letters[i]}${letters[i+1]}: ${delay}"
+
+echo "${id}"
+echo "${pw}"
+
+for ((i = 0; i < 19; i++)); do
+    #echo "${id:i:1}"
+    #delay=$((${id_millis[i+1]} - ${id_millis[i]}))
+    #echo " ${id:i:2}: ${delay}"
 done
 
 read  
