f6e3c7682c32105e6ceb5979eced973f0ea37a3c
190021081 <gjc6@st-andrews.ac.uk>
2021-10-30 12:54:49 +0100

    Added documents shortcut

--- BashedSolution.sh
+++ BashedSolution.sh
@@ -4,7 +4,7 @@ millis=()
 echo "Enter a password"
 # read a single character
 while read -n1 key; do
-    # if key = ENTER key
+    # if key = Escape key
     if [ $key == $'\e' ]; then
         break
     fi
@@ -16,3 +16,5 @@ for ((i = 0; i < ${#letters[@]} - 1; i++)); do
     delay=$((${millis[i+1]} - ${millis[i]}))
     echo "${letters[i]}${letters[i+1]}: ${delay}"
 done
+
+read  
